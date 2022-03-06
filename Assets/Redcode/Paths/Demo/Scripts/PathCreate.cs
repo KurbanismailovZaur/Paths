5859a23754529af1cb8c92b156470b627e8c14e2
Zaur Kurbanismailov <kurbanismailov.92@gmail.com>
2022-03-06 04:55:21 +0300

    Added new demo scene.

---
+++ Assets/Redcode/Paths/Demo/Scripts/PathCreate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paths.Demo
+{
+    public class PathCreate : MonoBehaviour
+    {
+        private void Start()
+        {
+            var path = Path.Create();
+            path.AddPoint(Vector3.zero);
+            path.AddPoint(new Vector3(1f, 2f, 0f));
+            path.AddPoint(new Vector3(2f, 0f, 0f));
+            path.AddPoint(new Vector3(0f, 1.25f, 0f));
+            path.AddPoint(new Vector3(2f, 1.25f, 0f));
+
+            path.Looped = true;
+        }
+    }
+}
